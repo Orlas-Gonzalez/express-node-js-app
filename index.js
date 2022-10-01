e449df84853b5ba76ad86844f747dbe9b1505580
Orlando González <orlando_garcia_glez@hotmail.com>
2022-10-01 15:02:36 -0500

    Actualización Del Proyecto, Actividad #7, 01/10/22

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
-const { pokemon } = require("./pokedex.json");
+const pokemon = require("./routes/pokemon");
 
 /*
     VERBOS HTTP
@@ -11,6 +12,7 @@ const { pokemon } = require("./pokedex.json");
     DELETE - Elimina un recurso
 */
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,39 +20,7 @@ app.get("/", (req, res, next) => {
   return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.post("/pokemon", (req, res, next) => {
-  return res.status(200).send(req.body.name);
-});
-
-app.get("/pokemon", (req, res, next) => {
-  return res.status(200).send(pokemon);
-});
-
-app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
-  const id = req.params.id - 1;
-  if (id >= 0 && id <= 150) {
-    return res.status(200).send(pokemon[req.params.id - 1]);
-  }
-  return res.status(404).send("Pokémon no encontrado");
-});
-
-/*
-    Operador Ternario 
-    Condición a evaluar ? valor si es verdadero : valor si es falso
-    NOTA: Un operador ternario por default retorna un valor
-*/
-
-app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
-  const name = req.params.name;
-  const pk = pokemon.filter((p) => {
-    return p.name.toUpperCase() == name.toUpperCase() && p;
-  });
-
-  if (pk.length > 0) {
-    return res.status(200).send(pk);
-  }
-  return res.status(404).send("Pokémon no encontrado.");
-});
+app.use("/pokemon", pokemon);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server Is Running...");
